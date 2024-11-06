ae83e5ce8ae307b58f12b23358778f073d797594
denikryt <danil.mirzoev@gmail.com>
2024-11-06 18:21:02 +0200

    fix

--- main.py
+++ main.py
@@ -1,51 +1,36 @@
 import os
 import discord
-from flask import Flask, jsonify, request
-from threading import Thread
-from discord import Intents, Client, Message, Forbidden, MessageType
-from discord.ext import commands
+from discord import Intents, Client
 from dotenv import load_dotenv
 from pathlib import Path
 
+# Load environment variables from the .env file
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-TOKEN_DISCORD = os.environ.get('TOKEN_DISCORD')
-
+# Retrieve the bot token and channel ID from environment variables
+DOOR_BOT_TOKEN = os.environ.get('DOOR_BOT_TOKEN')
 DISCORD_CHANNEL_ID_DOOR_EVENTS = os.environ.get('DISCORD_CHANNEL_ID_DOOR_EVENTS')
 
-# -----------Flask app Configuration -----------
-app = Flask('')
-
-# ----------- Discord Bot Configuration -----------
+# Discord Bot Configuration
 intents = Intents.default()
 intents.message_content = True 
 discord_client = Client(intents=intents)
 
-#------------------------------------------
-def send_new_message_to_discord_async():
-
-    text = ''
-
+# Function to send a message to the specified Discord channel
+async def send_new_message_to_discord():
+    text = 'hello'  # The message to send
     discord_channel = discord_client.get_channel(int(DISCORD_CHANNEL_ID_DOOR_EVENTS))
 
     if discord_channel:
-        # Ожидаем отправки сообщения и получаем объект отправленного сообщения
-        discord_channel.send(text)
+        await discord_channel.send(text)
         print(f'New message sent to discord')
-        
-#------------------------------------------
-@app.route('/')
-def home():
-    return 'Both bots are running'
 
-def run():
-    app.run(host="0.0.0.0", port=5000)
+# Event handler when the bot is ready
+@discord_client.event
+async def on_ready():
+    print(f'Bot has logged in as {discord_client.user}')
+    await send_new_message_to_discord()  # Send the message when the bot is ready
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-# Keep the server alive and run both bots
-keep_alive()
-discord_client.run(TOKEN_DISCORD)
+# Run the bot
+discord_client.run(DOOR_BOT_TOKEN)
